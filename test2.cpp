d11c74f19915300edcca1d05c4cd86154ac39490
bakukun <50573818+bakukun@users.noreply.github.com>
2023-01-12 14:17:49 +0900

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -20,4 +20,55 @@ void addKeyValues(leveldb::DB *db, std::ifstream& file) {
         string value;
         srand((unsigned int)time(NULL));
         int tmp = rand();
-                                                              1,1        꼭대기
+        tmp = (int)tmp % 10;
+        //0 < value <10 
+        if (tmp == 0) {
+                tmp = 1;
+        }
+
+
+        while (getline(file, line)) {
+            key = line;
+            value = to_string(tmp);
+            s=db->Put(leveldb::WriteOptions(), key, value);
+        }
+    } else {
+        cout << "File not opened" << endl;
+        return;
+    }
+    return;
+}
+
+// iterate through all key-value pairs and print them out
+void printAllKVPairs(leveldb::DB * db) {
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        cout << it->key().ToString() << ": " << it->value().ToString() << endl;
+    }
+    assert(it->status().ok());
+
+    delete it;
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+    assert(status.ok());
+
+    leveldb::Status s;
+
+    string filename = argv[1];
+    std::ifstream file(filename);
+
+    addKeyValues(db, file);  //custom function to load DB with key-value pairs
+    printAllKVPairs(db);  // custom function to print all key-value pairs
+    delete db;
+
+    return 0;
+}
+
+
+
